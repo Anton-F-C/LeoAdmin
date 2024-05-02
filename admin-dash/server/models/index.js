a2121eb7c24176d4e2c610b2b385826ace86ff7d
Council <acouncil@deloitte.com>
2024-05-01 23:46:46 -0700

    Fixed index.js

--- admin-dash/server/models/index.js
+++ admin-dash/server/models/index.js
@@ -4,8 +4,8 @@ import { Property } from './properties';
 // imported the user and properties models here and defined the associatians between them.
 
 // set up the associations here
-User.belongsToMany(User,{through: 'UserProperties', as: 'Properties'});// M:M properties to users
-Property.belongsToMany(Property,{through: 'UserProperties', as: 'Users'});// M:M users to properties
+User.belongsToMany(Property,{through: 'UserProperties', as: 'Properties'});// M:M properties to users
+Property.belongsToMany(User,{through: 'UserProperties', as: 'Users'});// M:M users to properties
 //multiple users can edit and manage properties. 
  
 
