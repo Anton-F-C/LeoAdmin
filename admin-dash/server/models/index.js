0d4ca3db908ab5cae9b88af1be91f50c150572ac
Council <acouncil@deloitte.com>
2024-05-01 23:38:42 -0700

    Updated index.js

--- admin-dash/server/models/index.js
+++ admin-dash/server/models/index.js
@@ -1,13 +1,13 @@
-const { User } = require('./users');
-const { Property } = require('./properties');
+import { User } from './users';
+import { Property } from './properties';
 
 // imported the user and properties models here and defined the associatians between them.
 
 // set up the associations here
-Property.belongsToMany(User,{through: 'UserProperties'});// M:M properties to users
-User.belongsToMany(Property,{through: 'UserProperties'});// M:M users to properties
+User.belongsToMany(User,{through: 'UserProperties', as: 'Properties'});// M:M properties to users
+Property.belongsToMany(Property,{through: 'UserProperties', as: 'Users'});// M:M users to properties
 //multiple users can edit and manage properties. 
  
 
 // and then export them all below
-module.exports = { User, Property }
+export { User, Property }
