52f5104d7771fcecb0acdd35db6325de8fb991e4
Council <acouncil@deloitte.com>
2024-05-19 01:54:36 -0700

    Fixed db on client side and conncected to front end.

--- admin-dash/client/src/sections/user/user-table-row.jsx
+++ admin-dash/client/src/sections/user/user-table-row.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-
-import Stack from '@mui/material/Stack';
-import Avatar from '@mui/material/Avatar';
 import Popover from '@mui/material/Popover';
 import TableRow from '@mui/material/TableRow';
 import Checkbox from '@mui/material/Checkbox';
@@ -10,21 +7,16 @@ import MenuItem from '@mui/material/MenuItem';
 import TableCell from '@mui/material/TableCell';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-
-import Label from '../../components/label';
 import Iconify from '../../components/iconify';
 
-// ----------------------------------------------------------------------
 
 export default function UserTableRow({
   selected,
   name,
-  avatarUrl,
-  company,
+  phone,
+  email,
   role,
-  isVerified,
-  status,
-  handleClick,
+  handleClick
 }) {
   const [open, setOpen] = useState(null);
 
@@ -44,23 +36,16 @@ export default function UserTableRow({
         </TableCell>
 
         <TableCell component="th" scope="row" padding="none">
-          <Stack direction="row" alignItems="center" spacing={2}>
-            <Avatar alt={name} src={avatarUrl} />
-            <Typography variant="subtitle2" noWrap>
-              {name}
-            </Typography>
-          </Stack>
+          <Typography variant="subtitle2" noWrap>
+            {name}
+          </Typography>
         </TableCell>
 
-        <TableCell>{company}</TableCell>
+        <TableCell>{phone}</TableCell>
+
+        <TableCell>{email}</TableCell>
 
         <TableCell>{role}</TableCell>
-
-        <TableCell align="center">{isVerified ? 'Yes' : 'No'}</TableCell>
-
-        <TableCell>
-          <Label color={(status === 'banned' && 'error') || 'success'}>{status}</Label>
-        </TableCell>
 
         <TableCell align="right">
           <IconButton onClick={handleOpenMenu}>
@@ -94,12 +79,10 @@ export default function UserTableRow({
 }
 
 UserTableRow.propTypes = {
-  avatarUrl: PropTypes.any,
-  company: PropTypes.any,
-  handleClick: PropTypes.func,
-  isVerified: PropTypes.any,
   name: PropTypes.any,
   role: PropTypes.any,
-  selected: PropTypes.any,
-  status: PropTypes.string,
+  phone: PropTypes.any,
+  email: PropTypes.any,
+  selected: PropTypes.bool,
+  handleClick: PropTypes.func,
 };
--- admin-dash/server/app.js
+++ admin-dash/server/app.js
@@ -1,14 +1,31 @@
-import express, { json } from "express";
+import express from "express";
+import userRoute from './routes/user.js';
+import propertyRoute from './routes/property.js';
+import cors from 'cors';
+import morgan from 'morgan';
+
 const app = express();
-import userRoute from '../server/routes/user.js';
-import propertyRoute from '../server/routes/property.js';
 
+app.use(cors());
+//logging middleware
+app.use(morgan('dev'));
+//parsing middleware from form input data  and json
+app.use(express.json()); //returns middleware 
+app.use(express.urlencoded({ extended: false }));
 
-app.use(json()); //returns middleware 
 app.use('/users', userRoute);
 app.use('/properties', propertyRoute);
 
+// 404 handler
+app.use((req, res) => {
+    res.status(404).send({error: '404 - Not Found', message: 'No route found for the requested URL'});
+});
 
-
+// error handling middleware
+app.use((error, req, res, next) => {
+    console.error('SERVER ERROR: ', error);
+    if(res.statusCode < 400) res.status(500);
+    res.send({error: error.message, name: error.name, message: error.message, table: error.table});
+});
 
 export default app;
